4de649fb790815d330d6a5f835de69b24f585a61
UMIDJON <umidjon@gmail.com>
2025-04-10 00:28:08 +0500

    done

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:welcome_product_ui/product_2.dart';
+import 'package:welcome_product_ui/pages/detail_page.dart';
 
 class Product1 extends StatefulWidget {
   const Product1({super.key});
